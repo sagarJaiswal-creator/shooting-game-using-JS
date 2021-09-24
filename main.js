da32ad82e91bb585bdfcf54ab55f61a5ab9591ae
Sagar jaiswal <79974954+sagarJaiswal-creator@users.noreply.github.com>
2021-09-24 20:07:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -101,6 +101,23 @@ class Particles {
   }
 }
 
+class Stars {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
@@ -118,6 +135,18 @@ function init() {
   initScore = 0;
   score.innerHtml = 0;
 }
+
+function spwanStars() {
+  for (let i = 0; i <= 10; i++) {
+    let radie = Math.random() * 5;
+    let xCoordinate = Math.random() * canvas.width;
+    let yCoordinate = Math.random() * canvas.height;
+
+    const star = new Stars(xCoordinate, yCoordinate, radie, "white");
+    star.draw();
+  }
+}
+
 
 function spwanEnemy() {
   setInterval((event) => {
@@ -148,6 +177,7 @@ function animate() {
   animationId = requestAnimationFrame(animate);
   ctx.fillStyle = "rgba(0,0,0,0.1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  spwanStars();
   player.draw();
 
   // adding particles on each frame at collison
